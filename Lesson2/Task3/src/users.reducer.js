814a2c4a7ca5889823935241e56c4150dd368e34
BohdanChernii <64136178+BohdanChernii@users.noreply.github.com>
2021-11-02 14:13:02 +0200

    ...

--- Lesson2/Task3/src/users.reducer.js
+++ Lesson2/Task3/src/users.reducer.js
@@ -4,17 +4,16 @@ const initialState = {
   usersList: [],
 };
 export const usersReducer = (state = initialState, action) => {
-  const { usersList } = state;
   switch (action.type) {
     case ADDUSER:
       return {
         ...state,
-        usersList: [usersList, action.userData],
+        usersList: [...state.usersList, action.userData],
       };
     case DELETEUSER:
       return {
         ...state,
-        usersList: usersList.filter(user => user.id !== action.id),
+        usersList: state.usersList.filter(user => user.id !== action.id),
       };
     default:
       return { state };
