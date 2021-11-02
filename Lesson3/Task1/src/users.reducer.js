df73fa1545d4bb9d9a0432eef8b4db0611cd0f5f
BohdanChernii <64136178+BohdanChernii@users.noreply.github.com>
2021-11-02 16:23:37 +0200

    Lesson3

---
+++ Lesson3/Task1/src/users.reducer.js
@@ -0,0 +1,21 @@
+import { ADDUSER, DELETEUSER } from './users.actions';
+
+const initialState = {
+  usersList: [],
+};
+export const usersReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADDUSER:
+      return {
+        ...state,
+        usersList: [...state.usersList, action.userData],
+      };
+    case DELETEUSER:
+      return {
+        ...state,
+        usersList: state.usersList.filter(user => user.id !== action.id),
+      };
+    default:
+      return state;
+  }
+};
---
+++ Lesson3/Task2/src/store.js
@@ -0,0 +1,48 @@
+import { createStore } from 'redux';
+
+const INCREMENT = 'COUNTER/INCREMENT';
+const DECREMENT = 'COUNTER/DECREMENT';
+const RESET = 'COUNTER/RESET';
+export const increment = () => {
+  return {
+    type: INCREMENT,
+  };
+};
+export const decrement = () => {
+  return {
+    type: DECREMENT,
+  };
+};
+export const reset = () => {
+  return {
+    type: RESET,
+  };
+};
+export const initialState = {
+  history: [],
+};
+const counterReducer = (state = initialState, action) => {
+  const { history } = state;
+
+  switch (action.type) {
+    case INCREMENT:
+      return {
+        ...state,
+        history: [...history, '+1'],
+      };
+    case DECREMENT:
+      return {
+        ...state,
+        history: history.concat(-1),
+      };
+    case RESET:
+      return {
+        ...state,
+        history: [],
+      };
+    default:
+      return state;
+  }
+};
+const store = createStore(counterReducer);
+export default store;
