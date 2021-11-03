8c7b28df0fecb32f60a1e596845c982989eefe33
BohdanChernii <64136178+BohdanChernii@users.noreply.github.com>
2021-11-03 10:07:32 +0200

    name

--- Lesson3/Task3/src/cart.actions.js
+++ Lesson3/Task3/src/cart.actions.js
@@ -1,10 +1,10 @@
 export const ADD_PRODUCT = 'CART/ADD_PRODUCT';
 export const REMOVE_PRODUCT = 'CART/REMOVE_PRODUCT';
 
-export const addProduct = (id, product) => {
+export const addProduct = (id, name) => {
   return {
     type: ADD_PRODUCT,
-    product,
+    name,
     id,
   };
 };
--- Lesson3/Task3/src/cart.reducer.js
+++ Lesson3/Task3/src/cart.reducer.js
@@ -8,7 +8,7 @@ export const cartReducer = (state = defaultCart, action) => {
     case ADD_PRODUCT:
       return {
         ...state,
-        products: [...state.products, action.id, action.product],
+        products: [...state.products, action.id, action.name],
       };
     case REMOVE_PRODUCT:
       const newList = state.products.filter(product => product.id !== action.userId);
